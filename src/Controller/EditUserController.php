d4189882d7c8dba3ccdd0e83ed57a1cdfdb26f5c
Lukas879-web <2056@htl.rennweg.at>
2026-03-25 12:54:32 +0100

    Benutzerverwaltung

---
+++ src/Controller/EditUserController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\User;
+use App\Form\BioType;
+use App\Form\UsernameType;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\UserEmailType;
+use Symfony\Component\HttpFoundation\Request;
+
+final class EditUserController extends AbstractController
+{   
+    #[Route('/edit/username', name: 'app_edit_username')]
+    public function editUsername(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(UsernameType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_user_management');
+        }
+
+        return $this->render('edit_user/edit_username.html.twig', [
+            'controller_name' => 'EditUserController',
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/edit/email', name: 'app_edit_email')]
+    public function editEmail(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(UserEmailType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_user_management');
+        }
+        else {
+            return $this->render('edit_user/edit_email.html.twig', [
+            'controller_name' => 'EditUserController',
+            'form' => $form->createView(),
+        ]);
+        }
+    }
+
+    #[Route('/edit/bio', name: 'app_edit_bio')]
+    public function editBio(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(BioType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_user_management');
+        }
+
+        return $this->render('edit_user/edit_bio.html.twig', [
+            'controller_name' => 'EditUserController',
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Controller/UserManagementController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+
+final class UserManagementController extends AbstractController
+{
+    #[Route('/user/management', name: 'app_user_management')]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        return $this->render('user_management/index.html.twig', [
+            'controller_name' => 'UserManagementController',
+            'user_data' => $user,
+        ]);
+    }
+}
